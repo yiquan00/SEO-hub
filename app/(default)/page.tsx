ce96ae4e235a22036ea3430ba6e7ccd0c8b1759a
yiquan00 <yiq.yan@foxmail.com>
2024-04-22 20:53:02 +0800

    Update page.tsx

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
   title: 'SEO tool hub:One-Stop Solution for all your SEO needs.',
-  description: 'SEO tool hub:Your One-Stop Solution for all your SEO needs. Includes keyword research, backlink analysis, and rank tracking tools to optimize your online presence.',
+  description: 'SEO tool hub is a One-Stop Solution for all your SEO needs. Includes keyword research, backlink analysis, and rank tracking tools ',
 }
 
 import Hero from '@/components/hero'
