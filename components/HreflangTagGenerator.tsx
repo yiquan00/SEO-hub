790355cc78246825925a172711b16eba22bfcf0b
yiquan00 <yiq.yan@foxmail.com>
2024-05-03 16:29:16 +0800

    Update HreflangTagGenerator.tsx

--- components/HreflangTagGenerator.tsx
+++ components/HreflangTagGenerator.tsx
@@ -31,7 +31,7 @@ export default function HreflangTagGenerator() {
     setConfigurations(updatedConfigurations);
   };
 
-  const updateConfiguration = (index, field, value) => {
+  const updateConfiguration = (index:number, field, value) => {
     const updatedConfigurations = [...configurations];
     updatedConfigurations[index][field] = value;
     setConfigurations(updatedConfigurations);
