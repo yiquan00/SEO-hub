f451f662336795713b9ddcb5b9c3f6231e96036e
yiquan00 <yiq.yan@foxmail.com>
2024-05-03 20:05:43 +0800

    fix bug

---
+++ components/11HreflangTagGenerator.tsx
@@ -0,0 +1,25 @@
+
+'use client'
+
+import { useState, useRef, useEffect } from 'react'
+import { Transition } from '@headlessui/react'
+import Image from 'next/image'
+import FeaturesBg from '@/public/images/features-bg.png'
+import FeaturesElement from '@/public/images/features-element.png'
+import languages from '@/components/languages'
+import Faq from '@/components/faq'
+
+
+export default function HreflangTagGenerator() {
+
+    return (
+    <section className="relative">
+     
+      <Faq />
+
+
+
+    </section>
+
+  );
+}
