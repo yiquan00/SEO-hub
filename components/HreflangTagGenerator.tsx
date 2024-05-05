1905081036272071ec010d6b44add8aa351d0505
yiquan00 <yiq.yan@foxmail.com>
2024-05-05 09:42:18 +0800

    Update HreflangTagGenerator.tsx

--- components/HreflangTagGenerator.tsx
+++ components/HreflangTagGenerator.tsx
@@ -172,11 +172,11 @@ const removeConfiguration = (index: number)  => {
 
     {showToast && (
 
-              <div id="toast-simple" class="fixed flex items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow top-20 right-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
-                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <div id="toast-simple" className="fixed flex items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow top-20 right-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
+                 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
         </svg>
-              <div class="ps-4 text-sm font-normal"> Copy  success.</div>
+              <div className="ps-4 text-sm font-normal"> Copy  success.</div>
 </div>
 
           )}
